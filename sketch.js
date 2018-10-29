7218f1da45ec19ee31d0f78d62063fc279d9c217
rachelebarazzetta <rachele.baraz@gmail.com>
2018-10-29 20:22:48 +0100

    final structure

--- sketch.js
+++ sketch.js
@@ -1,30 +1,81 @@
 var myData;
 var myData2;
+var myData3;
 var myImage1;
 var myImage2;
 var myImage3;
 var myImage4;
+var myImage5;
+var myImage6;
+var myImage7s;
 var mySong;
-
+var v = 0;
+var balls = [];
+var rx = 100;
+var ry = 200;
+var rw = 40;
+var rh = 150;
 
 function preload() {
   // put preload code here
   myData = loadJSON('./assets/pets-data.json');
   myData2 = loadJSON('./assets/petOfTheDay.json');
-  myImage3 = loadImage('./assets/images/cat-1.png');
-  myImage2 = loadImage('./assets/images/cat-2.png');
+  myData3 = loadJSON('./assets/pets-data2.json');
+
+  myImage3 = loadImage('./assets/images/gecko.png');
+  myImage2 = loadImage('./assets/images/cat-1.png');
   myImage1 = loadImage('./assets/images/dog-1.png');
   myImage4 = loadImage('./assets/images/dog-2.png');
+  myImage5 = loadImage('./assets/images/python.png');
+  myImage6 = loadImage('./assets/images/rabbit.png');
+  myImage7 = loadImage('./assets/images/goldfish.png');
+
   mySong = loadSound('./assets/victorySong.mp3')
 }
 
-var balls = [];
-
 function setup() {
   // put setup code here
+
   noStroke();
   createCanvas(windowWidth, windowHeight);
   background('lightgreen');
+
+
+}
+
+function draw() {
+  // put drawing code here
+
+  page01();
+  fill('black');
+  rect(rx, ry, rw, rh);
+
+  if (mouseIsPressed == true && mouseButton == RIGHT) {
+    page2();
+    fill('black');
+    rect(rx, ry, rw, rh);
+  } else if(mouseIsPressed == true && mouseButton == LEFT){
+    page01();
+    fill('black');
+    rect(rx, ry, rw, rh);
+  }
+
+  if (mouseX > rx && mouseX < (rx + rw) && mouseY > ry && mouseY < (ry + rh)) {
+    pageVictory();
+    fill('black');
+    rect(rx, ry, rw, rh);
+    if(mySong.isPlaying() == false){
+      mySong.loop();
+    }
+  } else {
+    if(mySong.isPlaying() == true){
+      mySong.pause();
+    }
+  }
+
+}
+
+function page01() {
 
   var newBall1 = new Ball(windowWidth / 2, windowHeight / 2, 200, myImage1, );
   balls.push(newBall1);
@@ -34,159 +85,276 @@ function setup() {
 
   var newBall2 = new Ball(windowWidth / 2 + 300, windowHeight / 2, 200, myImage3, );
   balls.push(newBall2);
+
+  background('lightgreen');
+
+  for (var j = 0; j < balls.length; j++) {
+    balls[j].display();
+  }
+
+  var x1 = width / 2;
+  var y1 = height / 2;
+  var radius = 100;
+
+  var name1 = myData.pets[1];
+  var name1_l = name1.name;
+
+  var species1 = myData.pets[1];
+  var species1_l = species1.species;
+
+  var birthYear1 = myData.pets[1];
+  var birthYear1_l = birthYear1.birthYear;
+
+  var favFoods1 = myData.pets[1];
+  var favFoods1_l = favFoods1.favFoods;
+
+
+  var d = dist(x1, y1, mouseX, mouseY);
+
+  if (d < radius) {
+    fill('black');
+    textSize(12);
+    text(name1_l, x1 + 20, y1 + 120);
+    text(species1_l, x1 + 20, y1 + 140);
+    text(birthYear1_l, x1 + 20, y1 + 160);
+    text(favFoods1_l, x1 + 20, y1 + 180);
+
+    text('Name = ', x1 - 50, y1 + 120);
+    text('Species = ', x1 - 50, y1 + 140);
+    text('Birth Year =', x1 - 50, y1 + 160);
+    text('Fav Food = ', x1 - 50, y1 + 180);
+
+    fill(255, 255, 255, 1);
+  } else if (d > radius) {
+    noFill();
+  }
+  ellipse(x1, y1, radius * 2);
+
+
+  var x2 = width / 2 + 300;
+  var y2 = height / 2;
+
+  var name2 = myData.pets[2];
+  var name2_l = name2.name;
+
+  var species2 = myData.pets[2];
+  var species2_l = species2.species;
+
+  var birthYear2 = myData.pets[2];
+  var birthYear2_l = birthYear2.birthYear;
+
+  var favFoods2 = myData.pets[2];
+  var favFoods2_l = favFoods2.favFoods;
+
+
+  var d = dist(x2, y2, mouseX, mouseY);
+
+  if (d < radius) {
+    fill('black');
+    text(name2_l, x2 + 20, y2 + 120);
+    text(species2_l, x2 + 20, y2 + 140);
+    text(birthYear2_l, x2 + 20, y2 + 160);
+    text(favFoods2_l, x2 + 20, y2 + 180);
+
+    text('Name = ', x2 - 50, y2 + 120);
+    text('Species = ', x2 - 50, y2 + 140);
+    text('Birth Year =', x2 - 50, y2 + 160);
+    text('Fav Food = ', x2 - 50, y2 + 180);
+    fill(255, 255, 255, 1);
+
+  } else {
+    noFill();
+  }
+  ellipse(x2, y2, radius * 2);
+
+  var x0 = width / 2 - 300;
+  var y0 = height / 2;
+
+  var name0 = myData.pets[0];
+  var name0_l = name0.name;
+
+  var species0 = myData.pets[0];
+  var species0_l = species0.species;
+
+  var birthYear0 = myData.pets[0];
+  var birthYear0_l = birthYear0.birthYear;
+
+  var favFoods0 = myData.pets[0];
+  var favFoods0_l = favFoods0.favFoods;
+
+
+  var d = dist(x0, y0, mouseX, mouseY);
+
+  if (d < radius) {
+
+    fill('black');
+    text(name0_l, x0 + 20, y0 + 120);
+    text(species0_l, x0 + 20, y0 + 140);
+    text(birthYear0_l, x0 + 20, y0 + 160);
+    text(favFoods0_l, x0 + 20, y0 + 180);
+
+    text('Name = ', x0 - 50, y0 + 120);
+    text('Species = ', x0 - 50, y0 + 140);
+    text('Birth Year =', x0 - 50, y0 + 160);
+    text('Fav Food = ', x0 - 50, y0 + 180);
+    fill(255, 255, 255, 1);
+  } else {
+    noFill();
+
+  }
+  ellipse(x0, y0, radius * 2);
 }
 
+function page2() {
 
+  var newBall3 = new Ball(windowWidth / 2, windowHeight / 2, 200, myImage5, );
+  balls.push(newBall3);
 
-function draw() {
-  // put drawing code here
-  if (mouseIsPressed == true) {
-    background('yellow');
-    push();
-    imageMode(CENTER);
-    translate(width / 2, height / 2);
-    rotate(frameCount / 100);
-    image(myImage4, 0, 0, 500, 500);
-    pop();
+  var newBall4 = new Ball(windowWidth / 2 - 300, windowHeight / 2, 200, myImage7, );
+  balls.push(newBall4);
 
-    if (mySong.isPlaying() == false) {
-      mySong.loop();
-    }
+  var newBall5 = new Ball(windowWidth / 2 + 300, windowHeight / 2, 200, myImage6, );
+  balls.push(newBall5);
 
+  background('red');
 
-    var name = myData2.name;
-    var species = myData2.species;
-    var age = myData2.age;
-    fill('black');
-    text('Pet of the day', width / 5 * 4, height / 2 - 80);
-    text('Name =', width / 5 * 4, height / 2 - 60);
-    text(name, width / 5 * 4, height / 2 - 40);
-    text('Species =', width / 5 * 4, height / 2 - 20);
-    text(species, width / 5 * 4, height / 2);
-    text('Age =', width / 5 * 4, height / 2 + 20);
-    text(age, width / 5 * 4, height / 2 + 40);
-  } else {
-    if (mySong.isPlaying() == true) {
-      mySong.pause();
-    }
-    background('lightgreen');
-
-    for (var j = 0; j < balls.length; j++) {
-      balls[j].display();
-    }
-
-    var x1 = width / 2;
-    var y1 = height / 2;
-    var radius = 100;
-
-    var name1 = myData.pets[1];
-    var name1_l = name1.name;
-
-    var species1 = myData.pets[1];
-    var species1_l = species1.species;
-
-    var birthYear1 = myData.pets[1];
-    var birthYear1_l = birthYear1.birthYear;
-
-    var favFoods1 = myData.pets[1];
-    var favFoods1_l = favFoods1.favFoods;
-
-
-    var d = dist(x1, y1, mouseX, mouseY);
-
-    if (d < radius) {
-      fill('black');
-      textSize(12);
-      text(name1_l, x1 + 20, y1 + 120);
-      text(species1_l, x1 + 20, y1 + 140);
-      text(birthYear1_l, x1 + 20, y1 + 160);
-      text(favFoods1_l, x1 + 20, y1 + 180);
-
-      text('Name = ', x1 - 50, y1 + 120);
-      text('Species = ', x1 - 50, y1 + 140);
-      text('Birth Year =', x1 - 50, y1 + 160);
-      text('Fav Food = ', x1 - 50, y1 + 180);
-
-      fill(255, 255, 255, 1);
-    } else if (d > radius) {
-      noFill();
-    }
-    ellipse(x1, y1, radius * 2);
-
-
-    var x2 = width / 2 + 300;
-    var y2 = height / 2;
-
-    var name2 = myData.pets[2];
-    var name2_l = name2.name;
-
-    var species2 = myData.pets[2];
-    var species2_l = species2.species;
-
-    var birthYear2 = myData.pets[2];
-    var birthYear2_l = birthYear2.birthYear;
-
-    var favFoods2 = myData.pets[2];
-    var favFoods2_l = favFoods2.favFoods;
-
-
-    var d = dist(x2, y2, mouseX, mouseY);
-
-    if (d < radius) {
-      fill('black');
-      text(name2_l, x2 + 20, y2 + 120);
-      text(species2_l, x2 + 20, y2 + 140);
-      text(birthYear2_l, x2 + 20, y2 + 160);
-      text(favFoods2_l, x2 + 20, y2 + 180);
-
-      text('Name = ', x2 - 50, y2 + 120);
-      text('Species = ', x2 - 50, y2 + 140);
-      text('Birth Year =', x2 - 50, y2 + 160);
-      text('Fav Food = ', x2 - 50, y2 + 180);
-      fill(255, 255, 255, 1);
-
-    } else {
-      noFill();
-    }
-    ellipse(x2, y2, radius * 2);
-
-    var x0 = width / 2 - 300;
-    var y0 = height / 2;
-
-    var name0 = myData.pets[0];
-    var name0_l = name0.name;
-
-    var species0 = myData.pets[0];
-    var species0_l = species0.species;
-
-    var birthYear0 = myData.pets[0];
-    var birthYear0_l = birthYear0.birthYear;
-
-    var favFoods0 = myData.pets[0];
-    var favFoods0_l = favFoods0.favFoods;
-
-
-    var d = dist(x0, y0, mouseX, mouseY);
-
-    if (d < radius) {
-
-      fill('black');
-      text(name0_l, x0 + 20, y0 + 120);
-      text(species0_l, x0 + 20, y0 + 140);
-      text(birthYear0_l, x0 + 20, y0 + 160);
-      text(favFoods0_l, x0 + 20, y0 + 180);
-
-      text('Name = ', x0 - 50, y0 + 120);
-      text('Species = ', x0 - 50, y0 + 140);
-      text('Birth Year =', x0 - 50, y0 + 160);
-      text('Fav Food = ', x0 - 50, y0 + 180);
-      fill(255, 255, 255, 1);
-    } else {
-      noFill();
-
-    }
-    ellipse(x0, y0, radius * 2);
+  for (var j = 0; j < balls.length; j++) {
+    balls[j].display();
   }
+
+  var x1 = width / 2;
+  var y1 = height / 2;
+  var radius = 100;
+
+  var name1 = myData3.pets[1];
+  var name1_l = name1.name;
+
+  var species1 = myData3.pets[1];
+  var species1_l = species1.species;
+
+  var birthYear1 = myData3.pets[1];
+  var birthYear1_l = birthYear1.birthYear;
+
+  var favFoods1 = myData3.pets[1];
+  var favFoods1_l = favFoods1.favFoods;
+
+
+  var d = dist(x1, y1, mouseX, mouseY);
+
+  if (d < radius) {
+    fill('black');
+    textSize(12);
+    text(name1_l, x1 + 20, y1 + 120);
+    text(species1_l, x1 + 20, y1 + 140);
+    text(birthYear1_l, x1 + 20, y1 + 160);
+    text(favFoods1_l, x1 + 20, y1 + 180);
+
+    text('Name = ', x1 - 50, y1 + 120);
+    text('Species = ', x1 - 50, y1 + 140);
+    text('Birth Year =', x1 - 50, y1 + 160);
+    text('Fav Food = ', x1 - 50, y1 + 180);
+
+    fill(255, 255, 255, 1);
+  } else if (d > radius) {
+    noFill();
+  }
+  ellipse(x1, y1, radius * 2);
+
+
+  var x2 = width / 2 + 300;
+  var y2 = height / 2;
+
+  var name2 = myData3.pets[2];
+  var name2_l = name2.name;
+
+  var species2 = myData3.pets[2];
+  var species2_l = species2.species;
+
+  var birthYear2 = myData3.pets[2];
+  var birthYear2_l = birthYear2.birthYear;
+
+  var favFoods2 = myData3.pets[2];
+  var favFoods2_l = favFoods2.favFoods;
+
+
+  var d = dist(x2, y2, mouseX, mouseY);
+
+  if (d < radius) {
+    fill('black');
+    text(name2_l, x2 + 20, y2 + 120);
+    text(species2_l, x2 + 20, y2 + 140);
+    text(birthYear2_l, x2 + 20, y2 + 160);
+    text(favFoods2_l, x2 + 20, y2 + 180);
+
+    text('Name = ', x2 - 50, y2 + 120);
+    text('Species = ', x2 - 50, y2 + 140);
+    text('Birth Year =', x2 - 50, y2 + 160);
+    text('Fav Food = ', x2 - 50, y2 + 180);
+    fill(255, 255, 255, 1);
+
+  } else {
+    noFill();
+  }
+  ellipse(x2, y2, radius * 2);
+
+  var x0 = width / 2 - 300;
+  var y0 = height / 2;
+
+  var name0 = myData3.pets[0];
+  var name0_l = name0.name;
+
+  var species0 = myData3.pets[0];
+  var species0_l = species0.species;
+
+  var birthYear0 = myData3.pets[0];
+  var birthYear0_l = birthYear0.birthYear;
+
+  var favFoods0 = myData3.pets[0];
+  var favFoods0_l = favFoods0.favFoods;
+
+
+  var d = dist(x0, y0, mouseX, mouseY);
+
+  if (d < radius) {
+
+    fill('black');
+    text(name0_l, x0 + 20, y0 + 120);
+    text(species0_l, x0 + 20, y0 + 140);
+    text(birthYear0_l, x0 + 20, y0 + 160);
+    text(favFoods0_l, x0 + 20, y0 + 180);
+
+    text('Name = ', x0 - 50, y0 + 120);
+    text('Species = ', x0 - 50, y0 + 140);
+    text('Birth Year =', x0 - 50, y0 + 160);
+    text('Fav Food = ', x0 - 50, y0 + 180);
+    fill(255, 255, 255, 1);
+  } else {
+    noFill();
+
+  }
+  ellipse(x0, y0, radius * 2);
+}
+
+function pageVictory() {
+  background('yellow');
+  push();
+  imageMode(CENTER);
+  translate(width / 2, height / 2);
+  rotate(frameCount / 100);
+  image(myImage4, 0, 0, 500, 500);
+  pop();
+
+  var name = myData2.name;
+  var species = myData2.species;
+  var age = myData2.age;
+  fill('black');
+  text('Pet of the day', width / 5 * 4, height / 2 - 80);
+  text('Name =', width / 5 * 4, height / 2 - 60);
+  text(name, width / 5 * 4, height / 2 - 40);
+  text('Species =', width / 5 * 4, height / 2 - 20);
+  text(species, width / 5 * 4, height / 2);
+  text('Age =', width / 5 * 4, height / 2 + 20);
+  text(age, width / 5 * 4, height / 2 + 40);
 }
 
 function Ball(_x, _y, _diameter, img, col) {
@@ -204,6 +372,10 @@ function Ball(_x, _y, _diameter, img, col) {
     fill('white');
     text(this.label, this.x, this.y);
   }
+}
+
+function click1() {
+  background('black');
 }
 
 function windowResized() {
